5edda176820da78fa3b07eae9e21d7de1a89882c
AmirjonKarimov2007 <amirjonkarimoff@gmail.com>
2025-12-27 17:59:53 +0500

    production

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,16 +15,16 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/logo.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/logo.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/logo.png",
+        media: "(prefers-color-scheme: dark)",
       },
     ],
     apple: "/apple-icon.png",
